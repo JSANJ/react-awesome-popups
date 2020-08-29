8080a6e8772be0b64163f1e7b227440fc4be53d8
Justin San Juan <jdqsj1997@yahoo.com>
2020-08-30 00:08:09 +0800

    stable package

--- demo/webpack.config.js
+++ demo/webpack.config.js
@@ -1,35 +1,19 @@
-var path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
 module.exports = {
-    mode: 'production',
-    entry: './src/index.jsx',
-    output: {
-        path: path.resolve('lib'),
-        filename: 'ReactAwesomePopups.js',
-        libraryTarget: 'commonjs2'
-    },
+    mode: 'development',
     module: {
         rules: [
             {
                 test: /\.jsx?$/,
                 exclude: /(node_modules)/,
                 use: 'babel-loader'
-            },
-            {
-                test: /\.css$/i,
-                use: ['style-loader','css-loader'],
-            },
-            {
-                test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf)(\?[a-z0-9=.]+)?$/,
-                loader: 'url-loader?limit=100000'
-            },
-            {
-                test: /\.svg$/,
-                use: ['@svgr/webpack'],
             }
         ]
     },
-    optimization: {
-        minimize: false
-    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ],
 }
